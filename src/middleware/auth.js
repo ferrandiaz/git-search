f7b3ca284cc17c41cbeff5a9eb0859defc2d6c39
IronMan <fdia13350@gmail.com>
2017-08-13 17:16:29 +0200

    Added new auth function

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,8 +3,13 @@
 const {server: { auth }} = require('../config');
 const basicAuth = require('express-basic-auth');
 
-const users = {};
+function authorizer (username, pass, cb) {
+  if(auth.user && auth.password){
+    if(auth.user === username && auth.pass === password) return cb(null, true);
+    else  return cb(null, false);
+  } else {
+    return cb(null, true);
+  }
+};
 
-users[auth.user] = auth.pass;
-
-module.exports.auth = basicAuth({users});
+module.exports.auth = basicAuth({authorizer: authorizer, authorizeAsync: true});
